5751a6fa03c8dee105fdad41d9e49e5120a67023
Georgios Kaleadis <georgios@kaleadis.de>
2019-08-22 06:25:53 +0200

    remove createPage in onCreateNode and activity timer

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,21 +5,21 @@ const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateNode = function onCreateNode({ actions, node, boundActionCreators }) {
-  const { createPage } = boundActionCreators;
+  // const { createPage } = boundActionCreators;
 
-  if (node.internal.type === `FakeData`) {
-    console.log('create new page');
+  // if (node.internal.type === `FakeData`) {
+  //   console.log('create new page');
 
-    const previewItemTemplate = path.resolve(`src/templates/preview-item.js`)
+  //   const previewItemTemplate = path.resolve(`src/templates/preview-item.js`)
 
-    createPage({
-      path: node.fields.slug,
-      component: previewItemTemplate,
-      context: {
-        slug: node.fields.slug, title: node.title
-      },
-    })
-  }
+  //   createPage({
+  //     path: node.fields.slug,
+  //     component: previewItemTemplate,
+  //     context: {
+  //       slug: node.fields.slug, title: node.title
+  //     },
+  //   })
+  // }
 }
 
 
@@ -44,14 +44,14 @@ exports.createPages = async function createPages({
   activity = reporter.activityTimer(`create pages`)
   activity.start();
 
-
   const totalPages = data.allFakeData.nodes.length;
+  console.log('createPages', totalPages)
 
   data.allFakeData.nodes.forEach((node, index) => {
     const { fields: {slug}, title } = node;
-    activity.setStatus(
-      `Creating ${index + 1} of ${totalPages} total pages`
-    );
+    // activity.setStatus(
+    //   `Creating ${index + 1} of ${totalPages} total pages`
+    // );
 
     createPage({
       path: slug,
